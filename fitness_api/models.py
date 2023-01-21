f76c6e4f6013daebf61f18d2a8cf0863d25a2a64
QGonz95 <q.gonz95@gmail.com>
2023-01-21 08:01:56 -0500

    push

--- fitness_api/models.py
+++ fitness_api/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
+class User(models.Model):
     first_name = models.CharField(max_length=150, blank=True)
     last_name = models.CharField(max_length=150, blank=True)
     email = models.CharField(max_length=255, blank=True, unique=True)
