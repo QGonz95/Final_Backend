572cc500fe188959040dd78902e4a7b783cefb5d
QGonz95 <q.gonz95@gmail.com>
2023-01-22 17:22:53 -0500

    edit

--- fitness_api/serializers.py
+++ fitness_api/serializers.py
@@ -8,8 +8,8 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
     def create(self, validated_data):
         user = User.objects.create(
-        firstname=validated_data['firstname'],
-        lastname=validated_data['lastname'],
+        first_name=validated_data['first_name'],
+        last_name=validated_data['last_name'],
         email=validated_data['email'],
         username=validated_data['username'],
         password= make_password(validated_data['password'])
